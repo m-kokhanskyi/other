52a126a281b6a15a906f20b5f1026a797b4c936f
m.kokhanskyi <maxim.kohanskij@outlook.com>
2022-04-15 09:55:51 +0200

    Create wrong_clasess.php

---
+++ wrong_clasess.php
@@ -0,0 +1,37 @@
+class Shape
+{
+            public $width;
+            public $height;
+ 
+            public function __construct($width, $height)
+            {
+                        $this->width = $width;
+                        $this->height = $height;
+            }
+}
+ 
+class Triangle
+{
+            public $radius;
+ 
+            public function __construct($radius)
+            {
+                        $this->radius = $radius;
+            }
+}
+ 
+class AreaCalculator
+{
+            public function calculate($shapes)
+            {
+                        foreach ($shapes as $shape) {
+            if (is_a($shape, 'Square')) {
+                                                $area[] = $shape->width * $shape->height;
+            } else if (is_a($shape, 'Triangle')) {
+                                                $area[] = $shape->radius * $shape->radius * pi();
+                                    }
+                        }
+ 
+            return array_sum($area);
+            }
+}
