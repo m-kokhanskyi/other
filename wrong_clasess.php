9f66dd7fef23197458795c93c488f3873c21cad7
m.kokhanskyi <maxim.kohanskij@outlook.com>
2022-04-15 09:58:04 +0200

    Update wrong_clasess.php

--- wrong_clasess.php
+++ wrong_clasess.php
@@ -1,37 +1,38 @@
+<?php 
+
 class Shape
 {
-            public $width;
-            public $height;
- 
-            public function __construct($width, $height)
-            {
-                        $this->width = $width;
-                        $this->height = $height;
-            }
+	public $width;
+	public $height;
+
+	public function __construct($width, $height)
+	{
+		$this->width = $width;
+		$this->height = $height;
+	}
 }
  
 class Triangle
 {
-            public $radius;
- 
-            public function __construct($radius)
-            {
-                        $this->radius = $radius;
-            }
+	public $radius;
+
+	public function __construct($radius)
+	{
+		$this->radius = $radius;
+	}
 }
  
 class AreaCalculator
 {
-            public function calculate($shapes)
-            {
-                        foreach ($shapes as $shape) {
-            if (is_a($shape, 'Square')) {
-                                                $area[] = $shape->width * $shape->height;
-            } else if (is_a($shape, 'Triangle')) {
-                                                $area[] = $shape->radius * $shape->radius * pi();
-                                    }
-                        }
- 
-            return array_sum($area);
-            }
+	public function calculate($shapes)
+	{
+		foreach ($shapes as $shape) {
+			if (is_a($shape, 'Square')) {
+				$area[] = $shape->width * $shape->height;
+			} elseif (is_a($shape, 'Triangle')) {
+				$area[] = $shape->radius * $shape->radius * pi();
+			}
+		}
+		return array_sum($area);
+	}
 }
