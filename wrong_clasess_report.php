f216dd0a3d2b52c92ca024170c969524b100758f
m.kokhanskyi <maxim.kohanskij@outlook.com>
2022-05-20 09:19:22 +0200

    Update wrong_clasess_report.php

--- wrong_clasess_report.php
+++ wrong_clasess_report.php
@@ -1,1 +1,36 @@
+<?php 
 
+class Report
+{
+    private Author $author;
+    private string $title;
+    private string $content;
+    private DateTime $date;
+ 
+    public function getData(): array
+    {
+        return [
+            'author' => $this->author->fullName(),
+            'title' => $this->title,
+            'content' => $this->content,
+            'timestamp' => $this->date->getTimestamp(),
+        ];
+    }
+ 
+    public function printJson(): string
+    {
+        return json_encode($this->getData());
+    }
+ 
+    public function printHtml(): string
+    {
+        return `<div>
+                    <h1>{$this->title}</h1>
+                    <div>
+                        <p>{$this->date->format('Y-m-d H:i:s')}</p>
+                        <p>{$this->author->fullName()}</p>
+                        <p>{$this->content}</p>
+                    </div>
+                </div>`;
+    }
+}
